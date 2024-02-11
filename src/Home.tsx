99d69b5b9970077cf08873d5c484125a30b5ef5c
ethanwalsh16 <ethancw16@gmail.com>
2024-02-10 20:46:28 -0500

    Update navlinks

--- src/Home.tsx
+++ src/Home.tsx
@@ -16,11 +16,11 @@ function App() {
 			<Header links={navlinks} />
 			<Hero />
 			<About aboutText={about} />
-			<div id="experience"><ExperienceHolder experiences={experiences}/></div>
-			<div id="projects"><ProjectsHolder projects={projects} /></div>
+			<div id="experience" className="experience"><ExperienceHolder experiences={experiences}/></div>
+			<div id="projects" className="projects"><ProjectsHolder projects={projects} /></div>
 			<div className="flex justify-center space-x-4 pt-8">
-				<div id="resume"><Resume /></div>
-				<div id="contact"><Contact /></div>
+				<div id="resume" className="resume"><Resume /></div>
+				<div id="contact" className="contact"><Contact /></div>
 			</div>
 			<Footer />
 		</>
