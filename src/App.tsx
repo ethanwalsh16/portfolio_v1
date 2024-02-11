44584e09a44d0b99a7f18066ef5f807e13299a7a
ethanwalsh16 <ethancw16@gmail.com>
2024-02-10 20:41:24 -0500

    Change to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./index.css";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import ResumePage from './pages/ResumePage'
 import Home from './Home'
 
