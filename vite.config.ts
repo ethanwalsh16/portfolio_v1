552ff58390ccd7e737cbbdfbf06e6a2113534c35
ethanwalsh16 <ethancw16@gmail.com>
2024-02-10 20:29:04 -0500

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    pluging: [react()],
-    base: './',
-  }
-  return config
+export default defineConfig({
+	plugins: [react()],
+	base: "/portfolio/"
 })
